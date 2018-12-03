eac8ea5b567023ceed8912da8828522b8dc50882
colinforzeal <rudenkopolina@gmail.com>
2018-12-03 23:12:27 +0300

    fix algorithm

--- src/App.js
+++ src/App.js
@@ -6,29 +6,32 @@ class App extends Component {
     popArray : [],
     optimum: 0,
     renderArr: [],
+    num: 20,
+    const: 0.02
   }
 
   componentWillMount() {
     this.Population();
+    this.main();
   }
 
   Population = () => {
-    const Pop = [];
     for ( let i = 0; i < 10; i++ ) {
       const kletka = {
-        gen: Math.floor(Math.random() * 31).toString(2),
+        gen: Math.floor(Math.random() * 32).toString(2),
       };
       switch (kletka.gen.length) {
         case 1: kletka.gen = '0000' + kletka.gen; break;
         case 2: kletka.gen = '000' + kletka.gen; break;
         case 3: kletka.gen = '00' + kletka.gen; break;
         case 4: kletka.gen = '0' + kletka.gen; break;
+        default:;
       }
-      kletka.fitness = parseInt(+(kletka.gen), 2);
-      Pop.push(kletka);
+      kletka.fitness = Math.abs(this.state.num - parseInt(+(kletka.gen), 2));
+      this.state.popArray.push(kletka);
+      this.state.renderArr.push(kletka);
     }
-    Pop.sort((a, b) => b.fitness - a.fitness);
-    this.setState({ popArray: Pop, renderArr: Pop });
+    this.state.popArray.sort((a, b) => a.fitness - b.fitness);
   }
 
   Rand = (arr) => {
@@ -46,46 +49,69 @@ class App extends Component {
     const newArr = arr;
     const mens = this.Rand(arr);
     const womens = this.Rand(arr);
+    let rand = [];
     for (let i = 0; i < 5; i++) {
+      rand.push(Math.floor(Math.random() * 6));
+      rand.push(Math.floor(Math.random() * 6));
+      rand.sort();
       const kletka = {
-        gen: mens[i].gen.substring(0,3) + womens[i].gen.substring(3),
+        gen: mens[i].gen.substring(0, rand[0])
+        + womens[i].gen.substring(rand[0], rand[1])
+        + mens[i].gen.substring(rand[1])
       }
-      kletka.fitness = parseInt(+(kletka.gen), 2);
+      kletka.fitness = Math.abs(this.state.num - parseInt(+(kletka.gen), 2));
       newArr.push(kletka);
+      rand = [];
     }
     return newArr;
   }
 
   Mutation = (arr) => {
-    const el = Math.floor(Math.random() * arr.length);
-    const mutGen = Math.floor(Math.random() * 5);
-    const newGen = Math.round(Math.random());
-    arr[el].gen = arr[el].gen.substring(0, mutGen) + newGen + arr[el].gen.substring(mutGen+1);
-    arr[el].fitness = parseInt(+(arr[el].gen), 2);
-    arr.sort((a, b) => b.fitness - a.fitness);
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < 5; j++) {
+        if (Math.random() < this.state.const) {
+          if (arr[i].gen.charAt(j) === '1') {
+            arr[i].gen = arr[i].gen.substring(0, j) + '0' + arr[i].gen.substring(j + 1)
+          } else {
+            arr[i].gen = arr[i].gen.substring(0, j) + '1' + arr[i].gen.substring(j + 1)
+          }
+        }
+      }
+      arr[i].fitness = Math.abs(this.state.num - parseInt(+(arr[i].gen), 2));
+    }
+    arr.sort((a, b) => a.fitness - b.fitness);
     return arr;
   }
 
   Otbor = (arr) => {
-    const newArr = arr;
+    const newArr = [ ...arr];
+    const randArr = [];
+    let j = 0;
+    for (let i = 0; i < newArr.length; i++) {
+      randArr.push(j + newArr[i].fitness);
+      j += 1;
+    }
+    for (let i = 0; i < 5; i++) {
+      let randNumber = Math.floor(Math.random() * randArr[newArr.length - 1]);
+      for (let k = 0; k < randArr.length; k++ ) {
+        if (randNumber < randArr[k]) {
+          newArr.splice(k, 1)
+          break;
+        }
+      }
+    }
     newArr.sort((a, b) => a.fitness - b.fitness);
-    for (let i = 0; i < 2; i++) {
-      newArr.splice(Math.floor(Math.random() * (5 - i)), 1)
-    }
-    for (let i = 0; i < 3; i++) {
-      newArr.splice(Math.floor(Math.random() * (12 - i)), 1)
-    }
-    newArr.sort((a, b) => b.fitness - a.fitness);
-    this.setState({ renderArr: this.state.renderArr.concat(newArr)})
+    this.setState({ renderArr: [ ...this.state.renderArr, ...newArr ]})
     return newArr;
   }
 
   main = () => {
     let opt = 0;
-    const skresPop = this.Skres(this.state.popArray);
-    const mutPop = this.Mutation(skresPop);
+    const skresPop = [ ...this.Skres(this.state.popArray)];
+    let mutPop = [...this.Mutation(skresPop)];
+    const newPop = this.Otbor(mutPop)
     this.setState({
-      popArray: this.Otbor(mutPop),
+      popArray: [...newPop],
     });
     for ( let j = 0; j < 10; j++) {
       opt = (opt + this.state.popArray[j].fitness);
@@ -97,14 +123,19 @@ class App extends Component {
 
   render() {
     console.log(this.state);
-    if (this.state.optimum < 30) {
-      this.main();
-    }
+     if (this.state.optimum > 1) {
+       this.main();
+     }
     return (
-      <div className="App">
-        {this.state.renderArr.map(() =>
-          <div>popa</div>
-        )}
+      <div className="app">
+        <span className="title"> Genetic Algorithm </span>
+        <button className="button" onClick={this.main}>{this.state.optimum}</button>
+        <div className="wrapper">
+          {this.state.renderArr.map(item =>
+            <div className={`kletka c${item.gen}`} />
+          )}
+        </div>
+        <span className="subtitle"> Postavte za4ot pliz </span>
       </div>
     );
   }
